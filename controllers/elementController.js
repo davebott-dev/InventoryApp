b7b1c2704bc36dc0a57f2c71254557645d34a2c8
= <bottenbergdave@gmail.com>
2024-09-09 22:27:55 -0400

    updated query and added another table in database

--- controllers/elementController.js
+++ controllers/elementController.js
@@ -4,5 +4,5 @@ exports.importData = async(req,res)=> {
     const index = req.params.categoryId;
     const category = req.params.category;
     const data = await db.getElementData(category,index);
-    res.send(data)
+    res.send(data);
 }
--- models/query.js
+++ models/query.js
@@ -1,26 +1,24 @@
 const pool = require("./pool");
 
 exports.getTrainerData = async () => {
-  const { rows } = await pool.query(
-    "SELECT * FROM trainers;"
-  );
+  const { rows } = await pool.query("SELECT * FROM trainers;");
   return rows;
 };
 exports.getSingleTrainerData = async (index) => {
-    const {rows} =await pool.query (
-        "SELECT * FROM trainers as t FULL JOIN pokemon as p ON t.trainer_id = p.owner_id WHERE t.trainer_id = ($1)",[index]
-    );
-    return rows;
-}
-exports.createNewTrainer = async (name) => {
-    await pool.query(
-        "INSERT INTO trainers (trainer_name) VALUES ($1)", [name]
-    );
-}
-exports.getElementData = async(category,index)=> {
-  const tableName = category;
-  const {rows} = await pool.query (
-    "SELECT * FROM " + tableName + " WHERE category_id = ($1)",[index]
+  const { rows } = await pool.query(
+    "SELECT trainer_id,p.category_id as pokemon_id,pokemon_name,p.category as pokemon_category,item_name,k.category as key_item_category, k.category_id as key_item_category_id FROM trainers as t LEFT JOIN pokemon as p ON t.trainer_id = p.owner_id LEFT JOIN key_items as k ON p.owner_id = k.key_item_id WHERE t.trainer_id = ($1)",
+    [index]
   );
   return rows;
-}
+};
+exports.createNewTrainer = async (name) => {
+  await pool.query("INSERT INTO trainers (trainer_name) VALUES ($1)", [name]);
+};
+exports.getElementData = async (category, index) => {
+  const tableName = category;
+  const { rows } = await pool.query(
+    "SELECT * FROM " + tableName + " WHERE category_id = ($1)",
+    [index]
+  );
+  return rows;
+};
