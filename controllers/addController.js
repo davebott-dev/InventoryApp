bf5af737b5843ccccc8613b4f6bfc670d687e62c
= <bottenbergdave@gmail.com>
2024-09-11 22:52:52 -0400

    renders add form on screen

---
+++ controllers/addController.js
@@ -0,0 +1,8 @@
+const db = require("../models/query");
+
+exports.importData = async (req, res) => {
+  const index = req.params.trainerId;
+  const data = await db.getSingleTrainerData(index);
+
+  res.render('add',{title:'Add Item Form', data:data});
+};
