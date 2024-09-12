2451327b73887e032e94ca19f494dc5e03105647
= <bottenbergdave@gmail.com>
2024-09-11 22:51:19 -0400

    fixed the controller to insert data into specific table

--- controllers/trainerController.js
+++ controllers/trainerController.js
@@ -3,14 +3,16 @@ const db = require("../models/query");
 exports.importData = async (req, res) => {
   const index = req.params.trainerId;
   const data = await db.getSingleTrainerData(index);
-
+  console.log(data)
   res.render("trainer", { trainerData: data});
 };
 exports.insertData = async(req,res)=> {
-  const category = req.body.category;
+  const category = req.body.categories;
   const name = req.body.categoryItem;
-  const index = req.params.categoryId;
+  const index = req.params.trainerId;
 
-  res.send(`category:${category}`)
+  await db.insertItem(category,name,index,category)
+
+  res.redirect(`/trainer/${index}`);
   
 }
