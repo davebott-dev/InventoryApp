0f8b927ba4bd458b97857d889d58a4f1ff8bd740
= <bottenbergdave@gmail.com>
2024-09-13 08:15:17 -0400

    added delete functionality

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,11 +2,15 @@ const db = require('../models/query');
 
 exports.importData = async(req,res) => {
     const data = await db.getTrainerData();
-    console.log(data)
     res.render('index',{trainers:data})
 }
 exports.insertData = async(req,res) => {
     const formData = req.body;
     await db.createNewTrainer(formData.text, formData.img);
+    res.redirect('/'); 
+}
+exports.deleteData = async(req,res) => {
+    const index = req.params.trainerId;
+    await db.deleteTrainer(index);
     res.redirect('/'); 
 }
