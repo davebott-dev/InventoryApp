e682834986a2ed14d78f470fa1402dc4fcb256c4
= <bottenbergdave@gmail.com>
2024-09-14 08:39:49 -0400

    added routes for update and delete path

--- routes/trainer.js
+++ routes/trainer.js
@@ -5,4 +5,8 @@ const controller = require('../controllers/trainerController');
 router.get('/:trainerId', controller.importData);
 router.post('/:trainerId/add',controller.insertData);
 
+router.get('/:trainerId/:category/:categoryId/update', controller.updateDataGet);
+router.post('/:trainerId/:category/:categoryId/update', controller.updateDataPost);
+
+router.post('/:trainerId/:category/:categoryId/delete', controller.deleteData);
 module.exports= router;
