9a9527cd2ce3d84ebd596ed215e0fbfcbe6e4430
= <bottenbergdave@gmail.com>
2024-09-15 18:47:11 -0400

    deployement fix

--- models/pool.js
+++ models/pool.js
@@ -1,9 +1,5 @@
 const {Pool} = require('pg');
 
 module.exports = new Pool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    database:process.env.DATABASE,
-    password:process.env.PASSWORD,
-    port:process.env.PORT
+connectionString: ProcessingInstruction.env.POSTGRES_URL 
 })
