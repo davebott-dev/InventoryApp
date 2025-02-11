16adab92c3bd46f480fcc3d34de6c01c0f8a5cc9
= <bottenbergdave@gmail.com>
2025-02-10 20:16:13 -0500

    vercel config

--- models/pool.js
+++ models/pool.js
@@ -1,9 +1,9 @@
+require('dotenv').config();
 const {Pool} = require('pg');
 
 module.exports = new Pool({
-    host:process.env.HOST,
-    user:process.env.USER,
-    database:process.env.DATABASE,
-    password:process.env.PASSWORD,
-    port:process.env.PORT
-})
+    connectionString: process.env.DB_SECRET,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
